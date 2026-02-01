d2a51e2beb4a029fd0aaa1edcd1a6a4d57900317
hiroyukisugiura-invitation <from.invitation@gmail.com>
2026-02-01 14:45:56 +0000

    feat: drag move for pieces

--- public/js/cobake-data.js
+++ public/js/cobake-data.js
@@ -1,12 +1,14 @@
 window.COBAKE_DATA = [
-  { id: "coin",   name: "Coin",   scale: 0.5823 },
-  { id: "coron",  name: "Coron",  scale: 1.7129 },
-  { id: "fuuu",   name: "Fuuu",   scale: 0.4557 },
-  { id: "monja",  name: "Monja",  scale: 1.9656 },
-  { id: "musshu", name: "Musshu", scale: 1.0173 },
-  { id: "pipple", name: "Pipple", scale: 1.0000 },
-  { id: "ruru",   name: "Ruru",   scale: 0.9586 },
-  { id: "vazuu",  name: "Vazuu",  scale: 0.6431 },
-  { id: "well",   name: "Well",   scale: 1.4581 },
-  { id: "wonka",  name: "Wonka",  scale: 1.2921 }
+  { id: "pipple", name: "Pipple", scale: 1.0 },
+
+  { id: "ruru",   name: "Ruru",   scale: 0.8 },
+  { id: "fuuu",   name: "Fuuu",   scale: 0.5 },
+  { id: "vazuu",  name: "Vazuu",  scale: 0.6 },
+  { id: "coin",   name: "Coin",   scale: 0.6 },
+
+  { id: "wonka",  name: "Wonka",  scale: 1.5 },
+  { id: "coron",  name: "Coron",  scale: 2.0 },
+  { id: "monja",  name: "Monja",  scale: 2.4 },
+  { id: "musshu", name: "Musshu", scale: 2.5 },
+  { id: "well",   name: "Well",   scale: 3.0 }
 ];
