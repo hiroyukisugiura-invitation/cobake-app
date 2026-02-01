eead6560ba7314a973282565c901c278db2f888a
hiroyukisugiura-invitation <from.invitation@gmail.com>
2026-02-01 10:20:25 +0000

    feat: drag move for pieces

---
+++ public/js/cobake-data.js
@@ -0,0 +1,12 @@
+window.COBAKE_DATA = [
+  { id: "coin",   name: "Coin" },
+  { id: "coron",  name: "Coron" },
+  { id: "fuuu",   name: "Fuuu" },
+  { id: "monja",  name: "Monja" },
+  { id: "musshu", name: "Musshu" },
+  { id: "pipple", name: "Pipple" },
+  { id: "ruru",   name: "Ruru" },
+  { id: "vazuu",  name: "Vazuu" },
+  { id: "well",   name: "Well" },
+  { id: "wonka",  name: "Wonka" }
+];
